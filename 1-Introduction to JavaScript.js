075c4da01618e0d8306412659663ef0d5499557b
itrikcode <kumarviveklohaman@gmail.com>
2024-01-04 21:06:56 +0530

    initial commit

---
+++ 1-Introduction to JavaScript.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+let x = 10;
+let y = 20;
+console.log(x+y);
+
+var a = 10;
+console.log("loder variable :",a);
+const b = 20;
+//b = a+x
+console.log("constent datatype ",b);
+
+let s = "Hello";
+console.log(s);
+let bool = true;
+console.log(bool);
+let number = [10,20,30];
+console.log(number);
+let str = ["AA","bb","ccc"];
+console.log(str[0]);
