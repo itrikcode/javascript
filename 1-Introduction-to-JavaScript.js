41dbaa622ba076c323a3bbe0f6489f8289116662
itrikcode <kumarviveklohaman@gmail.com>
2024-01-04 22:26:42 +0530

    complete if for while function experation === and ==

--- 1-Introduction-to-JavaScript.js
+++ 1-Introduction-to-JavaScript.js
@@ -57,3 +57,86 @@ let firstName = "vivek";
 let lastName = "kumar";
 let finalName = firstName + " " + lastName;
 console.log(finalName);
+
+// debuggin and console logging
+let x1= 10;
+let y2 = 20;
+console.log("Before addition - x:", x1, "y:", y2);
+
+let result = x1 + y2;
+console.log("After addition - Result:", result);
+
+//use try catch 
+let diviser =0;
+try{
+  // if(diviser==0){
+  //   throw new Error("connot divide by zero")
+  // }
+  debugger;
+  let result = 10/diviser;
+  console.log("Result :: ",result);
+}catch(ex){
+    console.error("Error ", ex.message);
+}
+
+// Function Expression:
+// A function expression is similar to a function declaration, but it's assigned to a variable. 
+//It can be named (in which case it's similar to a function declaration) or anonymous.
+
+let sum2 = function qq(a,b){
+  return a+b;
+}
+let resut= sum2(1,2);
+console.log(result);
+
+// Anonymous Function Expression:
+///An anonymous function expression is a function without a name.
+let anonymous = function(a,b){
+  return a-b;
+}
+console.log(anonymous(4,3));
+
+// Arrow Function:
+//Arrow functions provide a concise syntax, especially for short functions.
+
+let div =(a,b)=>a/b;
+console.log(div(4,2));
+
+function checkOadEven(a){
+  if(a%2==0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log(checkOadEven(1));
+
+///Function with Default Parameters:
+function power(base, exponent = 5){
+  return Math.pow(base,exponent)
+}
+
+console.log(power(2));
+console.log(power(2,3));
+
+// === and ==
+
+// === (Strict Equality):
+// The === operator, also known as the strict equality
+// operator, checks both value and type. It returns true if both operands are equal in value and type, and false otherwise.
+let num1 = 5;
+let num2 = "5";
+
+console.log(num1 === num2); // false, because the types are different
+
+// == (Equality):
+// The == operator, also known as the equality operator, checks only the value. 
+//It performs type coercion if the operands are of different types, trying to make them the same type before making the comparison.
+let num3 = 5;
+let num4 = "5";
+
+console.log(num3 == num4); // true, because the values are equal after type coercion
+
+
+
