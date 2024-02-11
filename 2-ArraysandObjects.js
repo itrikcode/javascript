bb7e110a1984b220f748b4e3512721a765eaeb28
itrikcode <kumarviveklohaman@gmail.com>
2024-02-11 14:20:32 +0530

    testing

--- 2-ArraysandObjects.js
+++ 2-ArraysandObjects.js
@@ -28,7 +28,7 @@ console.log(newArr);
 //shift(): Removes the first element from an array.
 newArr.shift();
 console.log(newArr);
-//[ 'vivek', 'ram' ]
+//[ 'vivek', 'ram' 'tes ]
 
 
 //splice(): Adds or removes elements at a specified index.
